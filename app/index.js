639290be36ababbbd71d5ca0a8a24f21045ea12a
burninggoose <gleb.andreev2000@gmail.com>
2019-06-01 11:06:46 +0300

    Added ability to create with argument

--- app/index.js
+++ app/index.js
@@ -2,35 +2,44 @@ const Generator = require('yeoman-generator');
 
 module.exports = class extends Generator {
 
+  constructor(args, opts) {
+    super(args, opts);
+
+    this.argument("name", { type: String, required: false });
+    this.name = this.options.name;
+  }
+
   async prompting() {
-    this.answers = await this.prompt([
-      {
-        type: "input",
-        name: "name",
-        message: "Your component name",
-        default: this.appname
-      }
-    ]);
-    this.answers.name = this.answers.name.charAt(0).toUpperCase() + this.answers.name.slice(1)
+    if (!this.name) {
+      this.answers = await this.prompt([
+        {
+          type: "input",
+          name: "name",
+          message: "Your component name",
+          default: this.appname
+        }
+      ]);
+      this.name = this.answers.name;
+    }
   }
 
   writing() {
     this.fs.copyTpl(
       this.templatePath('./index.js'),
-      this.destinationPath(`./${this.answers.name}/index.js`),
-      { name: this.answers.name }
+      this.destinationPath(`./${this.name}/index.js`),
+      { name: this.name }
     );
 
     this.fs.copyTpl(
       this.templatePath('./Component.jsx'),
-      this.destinationPath(`./${this.answers.name}/${this.answers.name}.jsx`),
-      { name: this.answers.name }
+      this.destinationPath(`./${this.name}/${this.name}.jsx`),
+      { name: this.name }
     );
 
     this.fs.copyTpl(
       this.templatePath('./Component.module.scss'),
-      this.destinationPath(`./${this.answers.name}/${this.answers.name}.module.scss`),
-      { name: this.answers.name }
+      this.destinationPath(`./${this.name}/${this.name}.module.scss`),
+      { name: this.name }
     );
   };
 
